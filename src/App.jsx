b36941b069e9e1ca7e7b18545f101284f6b0490f
javierjericm <javierjericm@gmail.com>
2025-08-21 22:11:17 +0800

    Added Search Implementation

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,13 @@ const App = () => {
   const [movieList, setMovieList] = useState([]);
   const [isloading, setIsLoading] = useState(false);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = "") => {
     setIsLoading(true);
     errorMessage("");
     try {
-      const endpoint = `{API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query
+        ? `{API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `{API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
       const response = await fetch(endpoint, API_OPTIONS);
 
@@ -51,8 +53,8 @@ const App = () => {
   };
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(searchTerm);
+  }, [searchTerm]);
 
   return (
     <main>
