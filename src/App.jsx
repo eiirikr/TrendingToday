e19cd88b820bd9d8bc6a38fe102190273129ec64
javierjericm <javierjericm@gmail.com>
2025-08-22 21:48:51 +0800

    Updated the codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
-import Search from "./components/Search";
-import Spinner from "./components/Spinner";
-import MovieCard from "./components/MovieCard";
+import Search from "./components/Search.jsx";
+import Spinner from "./components/Spinner.jsx";
+import MovieCard from "./components/MovieCard.jsx";
 import { useDebounce } from "react-use";
-import { getTrendingMovies, updateSearchCount } from "./appwrite";
+import { getTrendingMovies, updateSearchCount } from "./appwrite.js";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -13,7 +13,7 @@ const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: "Bearer ${API_KEY}",
+    Authorization: `Bearer ${API_KEY}`,
   },
 };
 
@@ -23,7 +23,7 @@ const App = () => {
 
   const [movieList, setMovieList] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
-  const [isloading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const [trendingMovies, setTrendingMovies] = useState([]);
 
@@ -31,11 +31,12 @@ const App = () => {
 
   const fetchMovies = async (query = "") => {
     setIsLoading(true);
-    errorMessage("");
+    setErrorMessage("");
+
     try {
       const endpoint = query
-        ? `{API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
-        : `{API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
       const response = await fetch(endpoint, API_OPTIONS);
 
@@ -57,7 +58,7 @@ const App = () => {
         await updateSearchCount(query, data.results[0]);
       }
     } catch (error) {
-      console.log(`Error fetching movies: ${error}`);
+      console.error(`Error fetching movies: ${error}`);
       setErrorMessage("Error fetching movies. Please try again later.");
     } finally {
       setIsLoading(false);
@@ -67,9 +68,10 @@ const App = () => {
   const loadTrendingMovies = async () => {
     try {
       const movies = await getTrendingMovies();
+
       setTrendingMovies(movies);
     } catch (error) {
-      console.log(`Error fetching trending movies: ${error}`);
+      console.error(`Error fetching trending movies: ${error}`);
     }
   };
 
@@ -83,32 +85,38 @@ const App = () => {
 
   return (
     <main>
-      <div className="pattern"></div>
+      <div className="pattern" />
+
       <div className="wrapper">
         <header>
-          <img src="./hero.png" alt="hero-banner" />
+          <img src="./hero.png" alt="Hero Banner" />
           <h1>
             Find <span className="text-gradient">Movies</span> You'll Enjoy
-            without the Hassle
+            Without the Hassle
           </h1>
+
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
+
         {trendingMovies.length > 0 && (
           <section className="trending">
             <h2>Trending Movies</h2>
+
             <ul>
-              {trendingMovies.map((movie, index) => {
+              {trendingMovies.map((movie, index) => (
                 <li key={movie.$id}>
                   <p>{index + 1}</p>
                   <img src={movie.poster_url} alt={movie.title} />
-                </li>;
-              })}
+                </li>
+              ))}
             </ul>
           </section>
         )}
+
         <section className="all-movies">
           <h2>All Movies</h2>
-          {isloading ? (
+
+          {isLoading ? (
             <Spinner />
           ) : errorMessage ? (
             <p className="text-red-500">{errorMessage}</p>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const MovieCard = ({
   movie: { title, vote_average, poster_path, release_date, original_language },
 }) => {
@@ -8,21 +6,25 @@ const MovieCard = ({
       <img
         src={
           poster_path
-            ? "htps://image.tmdb.org/t/p/w500/${poster_path}"
+            ? `https://image.tmdb.org/t/p/w500/${poster_path}`
             : "/no-movie.png"
         }
         alt={title}
       />
+
       <div className="mt-4">
         <h3>{title}</h3>
+
         <div className="content">
           <div className="rating">
             <img src="star.svg" alt="Star Icon" />
             <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
           </div>
-          <span>&U+2020</span>
+
+          <span>•</span>
           <p className="lang">{original_language}</p>
-          <span>&U+2020</span>
+
+          <span>•</span>
           <p className="year">
             {release_date ? release_date.split("-")[0] : "N/A"}
           </p>
@@ -31,5 +33,4 @@ const MovieCard = ({
     </div>
   );
 };
-
 export default MovieCard;
